2ccef1ff1dbe8aa5e42b29556ae1a48814c27da7
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2021-09-15 19:18:36 +0300

    Implementation of tasks 1 and 2 added

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
-
+const pipe = (...fns) =>{
+   fns.map(fn => {
+       if (typeof fn !== 'function') throw new TypeError('function expected');
+   })
+    return x => {
+        fns.map(fn => x = fn(x));
+        return x;
+    }
+} 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,22 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+    const composed = x => {
+        let res = x;
+        for (const fn of fns.reverse()) {
+            try {
+                res = fn(res);
+            } catch (error) {
+                composed['error'](error);
+                res = undefined
+                break;
+            }
+        }
+        return res;
+    }
+    composed.on = (error, cb) => composed[error] = cb;
+
+    return composed
+};
 
 module.exports = { compose };
